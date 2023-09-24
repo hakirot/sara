1d1891c4abc6ca14a14b90d67be57688abd71216
hakirot <hackirot@proton.me>
2023-09-24 14:34:52 -0700

    added init functions

--- sara.c
+++ sara.c
@@ -1,3 +1,18 @@
+
+/*
+  ███████╗    █████╗    ██████╗     █████╗    
+  ██╔════╝   ██╔══██╗   ██╔══██╗   ██╔══██╗   
+  ███████╗   ███████║   ██████╔╝   ███████║   
+  ╚════██║   ██╔══██║   ██╔══██╗   ██╔══██║   
+  ███████║██╗██║  ██║██╗██║  ██║██╗██║  ██║██╗
+  ╚══════╝╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝
+              SEMINAL ATMOSPHERE RANGING AREA
+*/                                           
+
+/*
+    (y,x)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
@@ -8,11 +23,18 @@ int main() {
 
   //int status = system("figlet -f 'ANSI Shadow' S.A.R.A");
   initscr();
-  printw("Hello World !!");
+  raw();
+  noecho(); // don't print input to screen
+
+  printw("S.A.R.A.");
+
   refresh();
   getch();
-  endwin();
 
+  refresh();
+  getch();
+
+  endwin();
 
   return 0;
 }

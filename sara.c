8f23a5f70d5a6b785bec8505f46b036ac87babb0
hakirot <hackirot@proton.me>
2023-09-24 19:49:03 -0700

    WORKING.

--- sara.c
+++ sara.c
@@ -6,31 +6,58 @@
 #include <string.h>
 #include <unistd.h>
 
-const char * title[7][100] = {
+// length 44 per line
+const char * title[7][99] = {
   { "███████╗    █████╗    ██████╗     █████╗    " },
   { "██╔════╝   ██╔══██╗   ██╔══██╗   ██╔══██╗   " },
   { "███████╗   ███████║   ██████╔╝   ███████║   " },
   { "╚════██║   ██╔══██║   ██╔══██╗   ██╔══██║   " },
   { "███████║██╗██║  ██║██╗██║  ██║██╗██║  ██║██╗" },
   { "╚══════╝╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝╚═╝  ╚═╝╚═╝" },
-  { "            SEMINAL ATMOSPHERE RANGING AREA " },
+  { "        SENTINELED ATMOSPHERIC RANGING AREA " },
 };
+
+int DELAY = 1500000;
 
 int main() {
 
-  setlocale(LC_ALL, ""); // Needed to print special characters
+  setlocale(LC_ALL, "");    // Needed to print special characters
 
-  initscr();
-  raw();                // Pass F1, ^C to program w/o signals
-                        // Also disables line buffering like cbreak()
-  noecho();             // Don't print input to screen
+  initscr();                // Initialize screen
+  //raw();                    // Pass F1, ^C to program w/o signals
+                            // Also disables line buffering like cbreak()
+  noecho();                 // Don't print input to screen
+  keypad(stdscr, TRUE);     // Enable reading of F1/2, arrow keys, etc
+  int row, col;             // Store the number of rows/cols
+  curs_set(FALSE);          // No cursor
 
-  keypad(stdscr, TRUE); // Enable reading of F1/2, arrow keys, etc
+  getmaxyx(stdscr,row,col); // Get total screen dimensions
+
 
   for(int i = 0; i < 7; i++){
-    printw("%s\n", title[i][0]);
+    //printw("%s\n", title[i][0]);
+    mvprintw(row/2 - 3 + i, (col-44)/2, "%s", title[i][0]);
     refresh();
     usleep(50000);      // Add some sexy timing
+  }
+
+  while(1){
+
+    usleep(DELAY);      // Add some sexy timing
+    clear();
+    for(int i = 0; i < 7; i++){
+      //printw("%s\n", title[i][0]);
+      mvprintw(row/2 - 4 + i, (col-44)/2, "%s", title[i][0]);
+      refresh();
+    }
+
+    usleep(DELAY);      // Add some sexy timing
+    clear();
+    for(int i = 0; i < 7; i++){
+      //printw("%s\n", title[i][0]);
+      mvprintw(row/2 - 3 + i, (col-44)/2, "%s", title[i][0]);
+      refresh();
+    }
   }
 
   refresh();

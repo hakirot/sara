179b5a96150524f3bb38045a71a22e698a72f9dd
hakirot <hackirot@proton.me>
2026-03-08 00:03:46 -0800

    remove excess neon

--- sara.c
+++ sara.c
@@ -502,8 +502,6 @@ void check_char(){
           }
         }
 
-        neon();
-
       } if (selection == choices[1]){
 
         char * file_path = "/sys/class/backlight/intel_backlight/brightness";

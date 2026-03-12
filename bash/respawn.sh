f250eb763b209518130b43c15e7308580a87fc22
hakirot <hackirot@proton.me>
2026-03-12 00:38:01 -0700

    more better newlook

--- bash/respawn.sh
+++ bash/respawn.sh
@@ -1,25 +1,12 @@
 #!/usr/bin/env bash
 
-xdotool key super+equal
-for i in {1..20}; do
-  xdotool key alt+x
-done
-
-# handle cmdline arg
-source $HOME/skps/newlook -r
-
-#xdotool key X
-#sleep .25
 xdotool key super+e
+source $HOME/skps/newlook -r
 xdotool key super+space
 xdotool key super+h
 xdotool key X
-xdotool key super+w
 sleep .25
-#sleep .5
-for i in {1..35}; do
-  xdotool key super+plus
-done
-for i in {1..10}; do
+
+for i in {1..6}; do
   xdotool key alt+z
 done

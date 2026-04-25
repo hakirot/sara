7abc50751a008c931ab44c752477099c1b14ee39
hakirot <hackirot@proton.me>
2026-04-25 16:12:47 -0700

    more reduction, minor animation tweak

--- animations.c
+++ animations.c
@@ -157,7 +157,7 @@ void neon(){
         if (IM_SET){
           print_overlay(im, 0);
         } else {
-          print_overlay(fg, 0);
+          print_overlay(fg, '-');
         }
       }
       second_frame = 1;
@@ -216,34 +216,13 @@ void neon_reverse(){
         attroff(COLOR_PAIR(FOREGROUND));
         attroff(A_BOLD);
 
-      } else { // screen is BIG
+      } else {
 
         if(IM_SET){
           print_bg();
-
           print_overlay(im, 0);
         } else {
-          // TODO all repeat code
-          for(int i = 0; i < FG_GLYPH_HEIGHT; i++){
-            mbstate_t state;
-            memset(&state, 0, sizeof(mbstate_t));
-            int iter_col = 0; // Track the column position
-            const char *iter_row = fg[i];
-            while (*iter_row) {
-              wchar_t wc;
-              size_t len = mbrtowc(&wc, iter_row, MB_CUR_MAX, &state); // Convert to wide char
-
-              if (*iter_row == ' '){
-                iter_row += len;
-                iter_col++;
-                continue;
-              }
-
-              mvaddch(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2 + iter_col, '-');
-              iter_row += len;
-              iter_col++;
-            }
-          }
+          print_overlay(fg, '-');
         }
       }
       refresh();

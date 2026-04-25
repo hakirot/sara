c6d7d6e7f8059e32c8c3a6409582f6585631881d
hakirot <hackirot@proton.me>
2026-04-25 02:24:10 -0700

    resolve segfault

--- animations.c
+++ animations.c
@@ -111,6 +111,10 @@ void neon(){
   clear();
   refresh();
 
+//char err[32];
+//sprintf(err, "%d", IM_SET);
+//error(err);
+
   while(cycle_length > elapsed_time){
 
     elapsed_time = (double)(clock() - cycle_start) / CLOCKS_PER_SEC;
@@ -161,28 +165,55 @@ void neon(){
         attroff(COLOR_PAIR(FOREGROUND));
       } else { // screen is BIG
 
-        // TODO: This code is repeated
-        for(int i = 0; i < FG_GLYPH_HEIGHT; i++){
-          mbstate_t state;
-          memset(&state, 0, sizeof(mbstate_t));
-          const char *iter_row = im[i];
-          int iter_col = 0; // Track the column position
+        if (IM_SET){
+          // TODO: This code is repeated
+          for(int i = 0; i < FG_GLYPH_HEIGHT; i++){
+            mbstate_t state;
+            memset(&state, 0, sizeof(mbstate_t));
+            const char *iter_row = im[i];
+            int iter_col = 0; // Track the column position
 
-          while (*iter_row) {
-            wchar_t wc;
-            size_t len = mbrtowc(&wc, iter_row, MB_CUR_MAX, &state); // Convert to wide char
+            while (*iter_row) {
+              wchar_t wc;
+              size_t len = mbrtowc(&wc, iter_row, MB_CUR_MAX, &state); // Convert to wide char
 
-            if (*iter_row == ' '){
+              if (*iter_row == ' '){
+                iter_row += len;
+                iter_col++;
+                continue;
+              }
+
+              cchar_t cchar;
+              setcchar(&cchar, &wc, 0, 0, NULL);
+              mvadd_wch(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2 + iter_col, &cchar);
               iter_row += len;
               iter_col++;
-              continue;
             }
+          }
+        } else {
+          // TODO: This code is repeated
+          for(int i = 0; i < FG_GLYPH_HEIGHT; i++){
+            mbstate_t state;
+            memset(&state, 0, sizeof(mbstate_t));
+            const char *iter_row = fg[i];
+            int iter_col = 0; // Track the column position
 
-            cchar_t cchar;
-            setcchar(&cchar, &wc, 0, 0, NULL);
-            mvadd_wch(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2 + iter_col, &cchar);
-            iter_row += len;
-            iter_col++;
+            while (*iter_row) {
+              wchar_t wc;
+              size_t len = mbrtowc(&wc, iter_row, MB_CUR_MAX, &state); // Convert to wide char
+
+              if (*iter_row == ' '){
+                iter_row += len;
+                iter_col++;
+                continue;
+              }
+
+              cchar_t cchar;
+              setcchar(&cchar, &wc, 0, 0, NULL);
+              mvadd_wch(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2 + iter_col, &cchar);
+              iter_row += len;
+              iter_col++;
+            }
           }
         }
       }
@@ -261,31 +292,65 @@ void neon_reverse(){
       clear();
       if (WIN_SIZE == NORMAL){
         attron(COLOR_PAIR(FOREGROUND));
-        for(int i = 0; i < 6; i++){
-          mvprintw(ROW/2 - 3 + i, (COL-GLYPH_LENGTH)/2, "%s", backdrop[i]);
+        if(use_bold_color_for_fg) attron(A_BOLD);
+        if(IM_SET){
+          for(int i = 0; i < FG_GLYPH_HEIGHT; i++){
+            mvprintw(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2, "%s", im[i]);
+          }
+        } else {
+          for(int i = 0; i < FG_GLYPH_HEIGHT; i++){
+            mvprintw(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2, "%c", ' ');
+          }
         }
         attroff(COLOR_PAIR(FOREGROUND));
+        attroff(A_BOLD);
+
       } else { // screen is BIG
 
-        for(int i = 0; i < BIG_GLYPH_HEIGHT; i++){
-
+        // TODO: This code is repeated?
+        for(int i = 0; i < FG_GLYPH_HEIGHT; i++){
           mbstate_t state;
           memset(&state, 0, sizeof(mbstate_t));
-          const char *iter_row = archsarazap[i];
           int iter_col = 0; // Track the column position
-          while (*iter_row) {
-            wchar_t wc;
-            size_t len = mbrtowc(&wc, iter_row, MB_CUR_MAX, &state); // Convert to wide char
 
-            cchar_t cchar;
-            setcchar(&cchar, &wc, 0, 0, NULL);
+          if(IM_SET){
+            const char *iter_row = im[i];
+            while (*iter_row) {
+              wchar_t wc;
+              size_t len = mbrtowc(&wc, iter_row, MB_CUR_MAX, &state); // Convert to wide char
 
-            is_char_in_search(wc, BG_STR) ? attron(COLOR_PAIR(BACKGROUND)) : attron(COLOR_PAIR(FOREGROUND)) ;
-            mvadd_wch(ROW/2 - 9 + i, (COL-GLYPH_LENGTH)/2 + iter_col, &cchar);
-            attroff(COLOR_PAIR(BACKGROUND));
-            attroff(COLOR_PAIR(FOREGROUND));
-            iter_row += len;
-            iter_col++;
+              if (*iter_row == ' '){
+                iter_row += len;
+                iter_col++;
+                continue;
+              }
+
+              cchar_t cchar;
+              setcchar(&cchar, &wc, 0, 0, NULL);
+              mvadd_wch(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2 + iter_col, &cchar);
+              iter_row += len;
+              iter_col++;
+            }
+          } else {
+            const char *iter_row = fg[i];
+            while (*iter_row) {
+              wchar_t wc;
+              size_t len = mbrtowc(&wc, iter_row, MB_CUR_MAX, &state); // Convert to wide char
+
+              if (*iter_row == ' '){
+                iter_row += len;
+                iter_col++;
+                continue;
+              }
+
+              wc = ' ';
+              cchar_t cchar;
+              setcchar(&cchar, &wc, 0, 0, NULL);
+              mvadd_wch(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2 + iter_col, &cchar);
+              iter_row += len;
+              iter_col++;
+            }
+
           }
         }
       }

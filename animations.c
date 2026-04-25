52b1b6d13066d2e035ccca90efd46ac8d6b337e4
hakirot <hackirot@proton.me>
2026-04-25 16:08:52 -0700

    reduce more

--- animations.c
+++ animations.c
@@ -221,34 +221,7 @@ void neon_reverse(){
         if(IM_SET){
           print_bg();
 
-          // TODO all repeat code
-          if(use_bold_color_for_fg) attron(A_BOLD);
-          attron(COLOR_PAIR(FOREGROUND));
-          for(int i = 0; i < FG_GLYPH_HEIGHT; i++){
-            mbstate_t state;
-            memset(&state, 0, sizeof(mbstate_t));
-            int iter_col = 0; // Track the column position
-
-            const char *iter_row = im[i];
-            while (*iter_row) {
-              wchar_t wc;
-              size_t len = mbrtowc(&wc, iter_row, MB_CUR_MAX, &state); // Convert to wide char
-
-              if (*iter_row == ' '){
-                iter_row += len;
-                iter_col++;
-                continue;
-              }
-
-              cchar_t cchar;
-              setcchar(&cchar, &wc, 0, 0, NULL);
-              mvadd_wch(ROW/2 - FG_GLYPH_HEIGHT/2 + fg_offset_y + i, (COL-FG_GLYPH_LENGTH)/2 + iter_col, &cchar);
-              iter_row += len;
-              iter_col++;
-            }
-          }
-          attroff(COLOR_PAIR(FOREGROUND));
-          attroff(A_BOLD);
+          print_overlay(im, 0);
         } else {
           // TODO all repeat code
           for(int i = 0; i < FG_GLYPH_HEIGHT; i++){

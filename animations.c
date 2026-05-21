41c77ede3007fd26ed7f72dc3dec346385ef3b3a
hakirot <hackirot@proton.me>
2026-05-20 20:47:20 -0700

    use menu functions for pshd window

--- animations.c
+++ animations.c
@@ -148,53 +148,6 @@ void _print_menu_borders(int dim_y, int dim_x, int offset_y, int offset_x){
   refresh();
 }
 
-void _print_pshd_borders(int dim_y, int dim_x, int offset_y, int offset_x){
-
-  if(dim_y < 3){
-    clear();
-    return;
-  }
-
-  // TODO: handle small size offsets
-  attron(COLOR_PAIR(FOREGROUND));
-  if(bold_color_menu) attron(A_BOLD);
-  wchar_t wc = MenuBorder[0];
-  cchar_t cchar;
-  setcchar(&cchar, &wc, 0, 0, NULL);
-  mvadd_wch(ROW/2 - dim_y/2 + offset_y, COL/2 - dim_x/2 + offset_x, &cchar);
-
-  wc = MenuBorder[1];
-  setcchar(&cchar, &wc, 0, 0, NULL);
-  mvadd_wch(ROW/2 - dim_y/2 + offset_y, COL/2 - dim_x/2 + dim_x - 1 + offset_x, &cchar);
-
-  wc = MenuBorder[2];
-  setcchar(&cchar, &wc, 0, 0, NULL);
-  mvadd_wch(ROW/2 - dim_y/2 + dim_y - 1 + offset_y, COL/2 - dim_x/2 + offset_x, &cchar);
-
-  wc = MenuBorder[3];
-  setcchar(&cchar, &wc, 0, 0, NULL);
-  mvadd_wch(ROW/2 - dim_y/2 + dim_y - 1 + offset_y, COL/2 - dim_x/2 + dim_x - 1 + offset_x, &cchar);
-
-  wc = MenuBorder[4];
-  setcchar(&cchar, &wc, 0, 0, NULL);
-  for(int i = 1; i < dim_x - 1; i++){
-    mvadd_wch(ROW/2 - dim_y/2 + offset_y, COL/2 - dim_x/2 + i + offset_x, &cchar);
-    mvadd_wch(ROW/2 - dim_y/2 + dim_y - 1 + offset_y, COL/2 - dim_x/2 + i + offset_x, &cchar);
-  }
-
-  wc = MenuBorder[5];
-  setcchar(&cchar, &wc, 0, 0, NULL);
-  for(int i = 1; i < dim_y - 1; i++){
-    mvadd_wch(ROW/2 - dim_y/2 + i + offset_y, COL/2 - dim_x/2 + offset_x, &cchar);
-    mvadd_wch(ROW/2 - dim_y/2 + i + offset_y, COL/2 - dim_x/2 + dim_x - 1 + offset_x, &cchar);
-  }
-  attroff(A_BOLD);
-  attroff(COLOR_PAIR(FOREGROUND));
-
-  _clear_pshd_window(dim_y, dim_x, offset_y, offset_x);
-  refresh();
-}
-
 void _print_confirm_window(char * command_str){
 
   clear();
@@ -306,23 +259,6 @@ void _print_confirm_selection(int selection){
 
 // TODO: this and _clear_pshd_window can be one function by passing pshd/menu offsets
 void _clear_menu(int dim_y, int dim_x, int offset_y, int offset_x){
-
-  if(dim_y < 3){
-    clear();
-    return;
-  }
-
-  wchar_t wc = L' ';
-  cchar_t cchar;
-  setcchar(&cchar, &wc, 0, 0, NULL);
-  for(int i = 1; i < dim_y - 1; i++){
-    for(int j = 1; j < dim_x - 1; j++){
-      mvadd_wch(ROW/2 - dim_y/2 + i + offset_y, COL/2 - dim_x/2 + j + offset_x, &cchar);
-    }
-  }
-}
-
-void _clear_pshd_window(int dim_y, int dim_x, int offset_y, int offset_x){
 
   if(dim_y < 3){
     clear();
